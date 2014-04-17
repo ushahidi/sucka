427b81d8d3713e655fc3d82317ece011cdcb59e7
Jonathon Morgan <jonathon@goodattheinternet.com>
2014-04-17 17:55:19 -0500

    refactored reliefweb

--- app/modules/suckas/reliefweb.js
+++ app/modules/suckas/reliefweb.js
@@ -1,14 +1,11 @@
-var Sucka = require("./sucka")
-  , logger = require("winston")
+var logger = require("winston")
   , moment = require("moment")
   , _ = require("underscore")
   , _s = require("underscore.string")
   , request = require("request");
 
-var ReliefSucka = function() {};
-ReliefSucka.prototype = Object.create(Sucka.prototype);
-
-ReliefSucka.definition = {
+var sucka = {};
+sucka.definition = {
   internalID: '883885b6-7baa-46c9-ad30-f4ccc0945674',
   sourceType: "reliefweb",
   frequency: "repeats",
@@ -17,10 +14,7 @@ ReliefSucka.definition = {
   endDate: moment('2014-04-05', 'YYYY-MM-DD')
 };
 
-
-ReliefSucka.prototype.suck = function() {
-  var that = this;
-
+sucka.suck = function(source, bus) {
   var url = 'http://api.rwlabs.org/v0/disaster/list';
   var propertiesObject = {
     fields: {
@@ -36,104 +30,77 @@ ReliefSucka.prototype.suck = function() {
     },
     limit: 100
   };
-  
-  if(!_.isUndefined(that.source.lastRetrieved)) {
+
+  if(!_.isUndefined(source.lastRetrieved)) {
+    var dateTime = new Date(source.lastRetrieved.publishedAt);
     propertiesObject.filter = {
       field: "date.created",
       value: {
-        from: (new Date(that.source.lastRetrieved.publishedAt)).getTime()
+        from: dateTime.getTime()
       }
     }
   }
 
-  var collectData = function(offset, memo, total) {
+  var collectData = function(offset, total, lastRetrievedDoc) {
     if(total && offset >= total) {
-      that.transform(memo);
+      bus.emit("sucked", source, lastRetrievedDoc);
       return;
     }
     
     request({url:url, qs:propertiesObject, json:true}, function(err, response, body) {
-      if(err) { that.handleError(err); return; }
+      if(err) { bus.emit('error', err); return; }
 
       var data = response.body.data;
 
-      collectData(offset + data.count, memo.concat(data.list), data.total);
-
-    });
-  };
-
-  collectData(0, []);
-};
-
-
-/**
- * Transform a list of tweets to items matching the CrisisNET schema.
- */
-ReliefSucka.prototype.transform = function(inputData) {
-  var that = this;
-
-  var outputData = [];
-
-  var transformItem = function(item) {
-    var returnData = {
-      remoteID: item.idToUse,
-      publishedAt: new Date(moment(item.fields.date.created)),
-      lifespan: "temporary",
-      content: item.fields.description || item.fields.name,
-      summary: item.fields.name,
-      geo: {
-        addressComponents: {
-          adminArea1: item.singleCountry
-        }
-      },
-      language: {
-        code: 'en',
-      },
-      source: "reliefweb",
-      tags: (function() {
-        return item.fields.type.map(function(tag) {
-          return { name: _s.slugify(tag.name), confidence: 1 }
+      _(data.list).each(function(item) {
+        _(item.fields.country).each(function(country, idx) {
+          item.singleCountry = country.name;
+    
+          // Every item must have a unique remote id, so because we create multiple
+          // documents from a single returned record, we need to create a modified
+          // remoteid for "child" documents
+          if(idx === 0) {
+            item.idToUse = item.id;
+          }
+          else {
+            item.idToUse = item.id + "-" + idx;
+          }
+          var transformed = sucka.transform(item);
+          bus.emit("data", transformed);
+          lastRetrievedDoc = transformed;
         });
-      })()
-    };
+      });
 
-    return returnData;
+      collectData(offset + data.count, data.total, lastRetrievedDoc);
 
+    });
   };
 
-
-  // Data from ReliefWeb can be associated with multiple countries. The 
-  // occurence of this disaster in each country is considered a separate 
-  // incident, we create a separate document.
-  _(inputData).each(function(item) {
-    _(item.fields.country).each(function(country, idx) {
-      item.singleCountry = country.name;
-      
-      // Every item must have a unique remote id, so because we create multiple
-      // documents from a single returned record, we need to create a modified
-      // remoteid for "child" documents
-      if(idx === 0) {
-        item.idToUse = item.id;
-      }
-      else {
-        item.idToUse = item.id + "-" + idx;
-      }
-
-      outputData.push(transformItem(item));
-    });
-  });
-
-  _(outputData).each(function(item) {
-    that.returnData(item);
-  });
-
-  var lastRetrievedDoc = _(_(outputData).sortBy(function(doc) {
-    return (new Date(doc.publishedAt)).getTime();
-  })).last();
-
-  that.allFinished(lastRetrievedDoc);
-
-  return outputData;
+  collectData(0, 0);
 };
 
-module.exports = ReliefSucka;
+sucka.transform = function(item) {
+  return {
+    remoteID: item.idToUse,
+    publishedAt: new Date(moment(item.fields.date.created)),
+    lifespan: "temporary",
+    content: item.fields.description || item.fields.name,
+    summary: item.fields.name,
+    geo: {
+      addressComponents: {
+        adminArea1: item.singleCountry
+      }
+    },
+    language: {
+      code: 'en',
+    },
+    source: "reliefweb",
+    tags: (function() {
+      return item.fields.type.map(function(tag) {
+        return { name: _s.slugify(tag.name), confidence: 1 }
+      });
+    })()
+  };
+};
+
+module.exports = sucka;
