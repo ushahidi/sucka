68c3cfd752ecdfdfb95e022b72c0cef2414492bd
Jonathon Morgan <jonathon@goodattheinternet.com>
2014-04-23 10:30:56 -0500

    adding ethnicity and knowngroup tags

--- bin/setup-system-tags.js
+++ bin/setup-system-tags.js
@@ -6,8 +6,9 @@ mongoose.connect(config.dbURI);
 var db = mongoose.connection;
 
 var conflictTags = require('../data/conflict-tags.json');
-
-var disasterTags = require('../data/disaster-tags.json');;
+var disasterTags = require('../data/disaster-tags.json');
+var enthnicityTags = require('../data/ethnicity-tags.json');
+var knowngroupTags = require('../data/knowngroup-tags.json');
 
 conflictTags = conflictTags.map(function(tag) {
   return {
@@ -23,7 +24,21 @@ disasterTags = disasterTags.map(function(tag) {
   }
 });
 
-var allTags = conflictTags.concat(disasterTags);
+enthnicityTags = enthnicityTags.map(function(tag) {
+  return {
+    name: tag,
+    categories: ['ethnicity']
+  }
+});
+
+knowngroupTags = knowngroupTags.map(function(tag) {
+  return {
+    name: tag,
+    categories: ['knowngroup']
+  }
+});
+
+var allTags = conflictTags.concat(disasterTags).concat(enthnicityTags).concat(knowngroupTags);
 
 db.once('open', function() {
   store.SystemTag.saveList(allTags, ['name']).then(function(tags) {
